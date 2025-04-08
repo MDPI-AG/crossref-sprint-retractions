8ecc6de45d935865e7e2da2eeca322fb124f60b5
Dietrich Rordorf <rordorf@mdpi.com>
2025-04-08 15:22:20 +0200

    feat(): pipeline to merge ROR data back into the RW data set

--- src/pipeline_ror.py
+++ src/pipeline_ror.py
@@ -5,15 +5,18 @@ against the ROR API.
 
 import os
 import requests
+import time
 import pandas as pd
+import numpy as np
 from datetime import datetime
 
 OUTPUT_DIR = "data"
 
 INPUT_PARQUET_ETL = os.path.join(OUTPUT_DIR, "retraction_watch_etl.parquet")
 OUTPUT_PARQUET_ETL = os.path.join(OUTPUT_DIR, "ror_etl.parquet")
+OUTPUT_CSV_ETL = os.path.join(OUTPUT_DIR, "ror_etl.csv")
 
-API_URL = "https://api.ror.org/organizations"
+API_URL = "https://api.ror.org/v2/organizations?query={query}"
 
 def load_parquet(file_path: str) -> pd.DataFrame:
     """
@@ -31,7 +34,14 @@ def save_parquet(df: pd.DataFrame, file_path: str) -> None:
     print(f"Saving DataFrame to {file_path}...")
     df.to_parquet(file_path, index=False)
     print(f"Saved DataFrame to {file_path}")
-    return df
+
+def save_csv(df: pd.DataFrame, file_path: str) -> None:
+    """
+    Save the DataFrame to a CSV file.
+    """
+    print(f"Saving DataFrame to {file_path}...")
+    df.to_csv(file_path, index=False)
+    print(f"Saved DataFrame to {file_path}")
 
 def get_ror_data(df_ror: pd.DataFrame, df_rw: pd.DataFrame) -> pd.DataFrame:
     """
@@ -44,26 +54,78 @@ def get_ror_data(df_ror: pd.DataFrame, df_rw: pd.DataFrame) -> pd.DataFrame:
     }
 
     # collect all 'institution' fields from RW data (lists) and remove duplicates
-    institutions = df_rw['institution'].drop_duplicates().tolist()
-    print(f"Found {len(institutions)} unique institutions in Retraction Watch data")
-    exit()
+    institutions = []
+    for index, row in df_rw.iterrows():
+        institutions.extend(row['institution'])
+
+    print(f"Institutions found in RW data: {len(institutions)}")
     
-    # Get the ROR data
-    response = requests.get(ror_url, headers=headers)
-    if response.status_code == 200:
-        ror_data = response.json()
-        print(f"Retrieved {len(ror_data)} rows from ROR API")
-    else:
-        print(f"Failed to retrieve ROR data. Status code: {response.status_code}")
-        return None
-    
-    # Convert the ROR data to a DataFrame
-    df_ror = pd.json_normalize(ror_data)
-    
-    # Merge the ROR data with the Retraction Watch data
-    df_merged = pd.merge(df_rw, df_ror, left_on='institution', right_on='name', how='left')
-    
-    return df_merged
+    # Convert numpy.ndarray elements to strings before creating a set
+    institutions = list(set(str(inst) if isinstance(inst, (list, np.ndarray)) else inst for inst in institutions))
+
+    # loop institutions and get ROR data if not yet in df_ror
+    counter = 0
+    progress = 0
+    for institution in institutions:
+        progress += 1
+
+        # dump progress every 100 iterations
+        if progress % 100 == 0:
+            print(f"Processed {progress} institutions...")
+
+        if institution in df_ror['raw'].values:
+            institutions.remove(institution)
+            continue
+
+        counter += 1
+        ror_url = API_URL.format(query=institution)
+        response = requests.get(ror_url, headers=headers)
+        if response.status_code == 200:
+            ror_data = response.json()
+            # check for ror_data.items and get first item
+            if 'items' in ror_data and isinstance(ror_data['items'], list) and len(ror_data['items']) > 0:
+                item = ror_data['items'][0]
+                
+                ror_id = item['id']
+                
+                name = None
+                if 'name' in item and len(item['name']) > 0:
+                    for name_obj in item['name']:
+                        if 'types' in name_obj and 'ror_display' in name_obj['types']:
+                            name = name_obj['value']
+                            break
+                
+                country = None
+                region = None
+                if 'locations' in item and len(item['locations']) > 0:
+                    location = item['locations'][0]['geonames_details']
+                    country = location['country_name']
+                    if 'country_subdivision_name' in location:
+                        region = location['country_subdivision_name']
+
+                new_row = pd.DataFrame([{
+                    'raw': institution,
+                    'ror': ror_id,
+                    'name': name,
+                    'country': country,
+                    'region': region
+                }])
+                df_ror = pd.concat([df_ror, new_row], ignore_index=True)
+            else:
+                print(f"No ROR data found for institution: {institution}")
+        
+        # throttle requests to avoid hitting the API too hard
+        if counter % 20 == 0:
+            print(f"Processed {counter} institutions, sleeping for 2 seconds...")
+            time.sleep(2)
+
+        # dump files every 100 iterations
+        if counter % 100 == 0:
+            print(f"Processed {counter} institutions, saving intermediate results...")
+            save_parquet(df_ror, OUTPUT_PARQUET_ETL)
+            save_csv(df_ror, OUTPUT_CSV_ETL)
+
+    return df_ror
 
 def main():
     if not os.path.exists(INPUT_PARQUET_ETL):
@@ -74,13 +136,15 @@ def main():
     if os.path.exists(OUTPUT_PARQUET_ETL):
         df_ror = load_parquet(OUTPUT_PARQUET_ETL)
     else:
-        df_ror = pd.DataFrame()
+        # create a df with columns name, ror
+        df_ror = pd.DataFrame(columns=['raw', 'ror', 'name', 'country', 'region'])
     
     # Match ROR IDs for the instituions data from RW
     df_ror = get_ror_data(df_ror, df_rw)
     
     # Save the merged data to a Parquet file
     save_parquet(df_ror, OUTPUT_PARQUET_ETL)
+    save_csv(df_ror, OUTPUT_CSV_ETL)
 
 if __name__ == "__main__":
     main()
---
+++ src/pipeline_rw_ror.py
@@ -0,0 +1,92 @@
+"""
+This script implement a pipeline to insert ROR IDs from the pipeline_ror back into the
+Retraction Watch data set.
+"""
+import os
+import pandas as pd
+
+INOUT_DIR = "data"
+INPUT_ROR_PARQUET = os.path.join(INOUT_DIR, "ror_etl.parquet")
+OUTPUT_RW_PARQUET = os.path.join(INOUT_DIR, "retraction_watch_etl.parquet")
+OUTPUT_RW_CSV = os.path.join(INOUT_DIR, "retraction_watch_etl.csv")
+
+def load_parquet(file_path: str):
+    """
+    Load the Parquet file into a pandas DataFrame.
+    """
+    print(f"Loading Parquet file from {file_path}...")
+    df = pd.read_parquet(file_path)
+    print(f"Loaded {len(df)} rows from {file_path}")
+    return df
+
+def save_parquet(df, file_path: str):
+    """
+    Save the DataFrame to a Parquet file.
+    """
+    print(f"Saving DataFrame to {file_path}...")
+    df.to_parquet(file_path, index=False)
+    print(f"Saved DataFrame to {file_path}")
+
+def save_csv(df, file_path: str):
+    """
+    Save the DataFrame to a CSV file.
+    """
+    print(f"Saving DataFrame to {file_path}...")
+    df.to_csv(file_path, index=False)
+    print(f"Saved DataFrame to {file_path}")
+
+def merge_rors_with_rw(df_ror, df_rw):
+    # loop RW data and check if the institution is in the ROR data
+    # and if so, add the ROR ID to the RW data
+    print("Merging ROR data with Retraction Watch data...")
+
+    fields  = ['rorids', 'rornames', 'rorcountries', 'rorregions']
+    for field in fields:
+        if field not in df_rw.columns:
+            df_rw[field] = [[] for _ in range(len(df_rw))]
+
+    for index, row in df_rw.iterrows():
+        institutions = row['institution']
+        rorids = list(row['rorids'])
+        rornames = list(row['rornames'])
+        rorcountries = list(row['rorcountries'])
+        rorregions = list(row['rorregions'])
+
+        for institution in institutions:
+            if institution in df_ror['raw'].values:
+                ror_row = df_ror.loc[df_ror['raw'] == institution]
+                rorids.append(ror_row['ror'].values[0])
+                rornames.append(ror_row['name'].values[0])
+                rorcountries.append(ror_row['country'].values[0])
+                rorregions.append(ror_row['region'].values[0])
+                
+        rorids = list(set(rorids))
+        df_rw.at[index, 'rorids'] = rorids
+        rornames = list(set(rornames))
+        df_rw.at[index, 'rornames'] = rornames
+        rorcountries = list(set(rorcountries))
+        df_rw.at[index, 'rorcountries'] = rorcountries
+        rorregions = list(set(rorregions))
+        df_rw.at[index, 'rorregions'] = rorregions
+
+    return df_rw
+
+def main():
+    """
+    Main function to run the pipeline.
+    """
+    # Load the Retraction Watch data
+    df_rw = load_parquet(OUTPUT_RW_PARQUET)
+
+    # Load the ROR data
+    df_ror = load_parquet(INPUT_ROR_PARQUET)
+
+    # Merge the dataframes on the 'rorids' column
+    df_rw = merge_rors_with_rw(df_ror, df_rw)
+
+    # Save the merged dataframe
+    save_parquet(df_rw, OUTPUT_RW_PARQUET)
+    save_csv(df_rw, OUTPUT_RW_CSV)
+    
+if __name__ == "__main__":
+    main()
